a72fd2b17babedb7c9974c7cf7b3ea298cb80661
Hunar Ahmad <39065447+hunar4321@users.noreply.github.com>
2020-08-31 17:00:05 +0300

    Update very_simple_GA.py

--- very_simple_GA.py
+++ very_simple_GA.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-@author: Hunar @ Brainxyz
-"""
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -15,39 +11,36 @@ mutation_rate = 0.03
 w1 = np.random.uniform(-1,1, (inp, hidden))
 w2 = np.random.uniform(-1,1, (hidden, out))
 
-## feedforward 
 def feedforward(X, w1, w2):
     z = np.tanh(X @ w1)
     yh = z @ w2
     return yh
 
-errs = []; # to store errs and track perforamnce
+errs = []; # to store errors and track performance
 for i in range(1000):
     
-    ## mutate the weights
-    m_w1 = w1 + np.random.uniform(-1,1, (w1.shape)) * mutation_rate
-    m_w2 = w2 + np.random.uniform(-1,1, (w2.shape)) * mutation_rate
+    ## mutate the weights by adding small random values
+    dw1 = np.random.uniform(-1,1, (w1.shape))
+    dw2 = np.random.uniform(-1,1, (w2.shape))
+    m_w1 = w1 + dw1 * mutation_rate
+    m_w2 = w2 + dw2 * mutation_rate
     
     ## feedforward1 - father network
     yh = feedforward(X, w1, w2);
     err_father = np.sum(np.abs(y - yh.ravel()));
 
-    ## feedforward2 - child network
+    ## feedforward2 - child network (mutated weights)
     yh = feedforward(X, m_w1, m_w2);
     err_child = np.sum(np.abs(y - yh.ravel()));
     
-    ## if child net performance better than father then update
+    ## if the child net performance is better than father then update weights
     if(err_child < err_father):
         w1 = m_w1;
         w2 = m_w2;
-        
     errs.append(err_child)
-        
+## check the results        
 plt.figure(1)
 plt.plot(errs)
 plt.title('err')
-
-## check 
 print(feedforward(X, w1, w2))
-        
     
