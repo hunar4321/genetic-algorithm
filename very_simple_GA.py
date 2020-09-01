c3280bb43ac9201fce424403fcdf463480e5471f
Hunar Ahmad <39065447+hunar4321@users.noreply.github.com>
2020-09-01 18:05:15 +0300

    Update very_simple_GA.py

--- very_simple_GA.py
+++ very_simple_GA.py
@@ -2,16 +2,10 @@
 @author: Hunar @ Brainxyz
 below is a very simple implementation of a GA based neural network which solves the XOR problem
 """
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-## XOR data
-X=np.asarray([[0,0],[1,1],[0,1],[1,0]]);
-y=np.asarray([0,0,1,1]);
-
-## net structure and initialization
+## net structure and initialization (single layer neural network)
 inp = 2; hidden = 5; out = 1;
 mutation_rate = 0.03
 w1 = np.random.uniform(-1,1, (inp, hidden))
@@ -22,31 +16,41 @@ def feedforward(X, w1, w2):
     yh = z @ w2
     return yh
 
-errs = []; # to store errors and track performance
-for i in range(1000):
-    
-    ## mutate the weights by adding small random values
-    dw1 = np.random.uniform(-1,1, (w1.shape))
-    dw2 = np.random.uniform(-1,1, (w2.shape))
-    m_w1 = w1 + dw1 * mutation_rate
-    m_w2 = w2 + dw2 * mutation_rate
-    
-    ## feedforward1 - father network
-    yh = feedforward(X, w1, w2);
-    err_father = np.sum(np.abs(y - yh.ravel()));
+def mutate(w, mutation_rate):
+    ## mutate the weights by adding small random values to them
+    dw = np.random.uniform(-1,1, (w.shape)) * mutation_rate
+    m_w = w + dw
+    return m_w
+       
+def train(X, y, w1, w2, iterations):
+    errs = []
+    for i in range(iterations):
+        
+        m_w1 = mutate(w1, mutation_rate) #mutate input-hidden weights
+        m_w2 = mutate(w2, mutation_rate) #mutate hidden-output weights
+        
+        yh1 = feedforward(X,   w1,   w2); #parent network
+        yh2 = feedforward(X, m_w1, m_w2); #child network (mutated weights)
+        
+        err_father = np.sum(np.abs(y - yh1.ravel())); #evaluate parent
+        err_child =  np.sum(np.abs(y - yh2.ravel())); #evaluate child 
+        
+        if(err_child < err_father): # select better
+            w1 = m_w1;
+            w2 = m_w2;   
+            
+        errs.append(err_child)        
+    return errs, w1, w2
 
-    ## feedforward2 - child network (mutated weights)
-    yh = feedforward(X, m_w1, m_w2);
-    err_child = np.sum(np.abs(y - yh.ravel()));
-    
-    ## if the child net performance is better than father then update weights
-    if(err_child < err_father):
-        w1 = m_w1;
-        w2 = m_w2;
-    errs.append(err_child)
-## check the results        
+## usage example: XOR data
+X=np.asarray([[0,0],[1,1],[0,1],[1,0]]);
+y=np.asarray([0,0,1,1]);
+
+iterations = 1000 
+errs, m_w1, m_w2 = train(X, y, w1, w2, iterations)
+
+## check the results  
+print(feedforward(X, m_w1, m_w2))      
 plt.figure(1)
 plt.plot(errs)
-plt.title('err')
-print(feedforward(X, w1, w2))
-    
+plt.title('errors')
